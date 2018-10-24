0a523d75b664a263ef16cb6bacb4fba67ed11198
ly-kris <847461291@qq.com>
2018-10-24 15:14:52 +0800

    commit message

---
+++ src/components/Todoitem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li class="item" @click="handleDelete">{{content}}</li>
+</template>
+<script>
+export default {
+  props: ['content', 'index'],
+  methods: {
+    handleDelete () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+<style scoped>
+  .item {
+    color: green;
+  }
+</style>
